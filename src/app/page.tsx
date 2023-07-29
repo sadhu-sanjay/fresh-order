ff9d8c63ab81e00a6527484e7b753af8a9865cbc
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-07-29 22:52:15 +0530

    remormated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import Image from 'next/image'
-import { Heading } from '~/components/atoms/Heading'
-import { Inter } from 'next/font/google'
-import { featuresLargeData, heroProps, twoColumnImageTextData } from '~/data/data'
-import { FeaturesLarge } from '~/components/sections/FeatursLarge'
-import { TwoColumnImageText } from '~/components/sections/TwoColumnImageText'
-import CTA from '~/components/atoms/CTA'
-
+import Image from "next/image";
+import { Heading } from "~/components/atoms/Heading";
+import { Inter } from "next/font/google";
+import {
+  featuresLargeData,
+  heroProps,
+  twoColumnImageTextData,
+} from "~/data/data";
+import { FeaturesLarge } from "~/components/sections/FeatursLarge";
+import { TwoColumnImageText } from "~/components/sections/TwoColumnImageText";
+import CTA from "~/components/atoms/CTA";
 
 export default function Home() {
   return (
-    <main >
+    <main>
       {/* <FeaturesLarge  {...featuresLargeData} /> */}
       <TwoColumnImageText {...twoColumnImageTextData} />
-        {/* <h1 className="text-6xl font-bold text-gray-900 "> Sanjay Goswami</h1> */}
+      {/* <h1 className="text-6xl font-bold text-gray-900 "> Sanjay Goswami</h1> */}
     </main>
-  )
+  );
 }
-
